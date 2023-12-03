ecc051ba4c8d5a563fa9a22235570cf70fa15767
Tauseef Akhtar Yusufzai <138979340+Tauseefyusufzai@users.noreply.github.com>
2023-12-03 19:01:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+function submitOrder() {
+    // Get user input
+    const imageInput = document.getElementById('image-upload');
+    const detailsInput = document.getElementById('details');
+
+    const imageFile = imageInput.files[0];
+    const details = detailsInput.value;
+
+    // You can perform additional validation or processing here before submitting the order
+
+    // For demonstration purposes, let's log the details to the console
+    console.log('Image File:', imageFile);
+    console.log('Details:', details);
+
+    // You can send the data to a server for further processing and order fulfillment
+    // Example: Use fetch() or XMLHttpRequest to send data to a server
+
+    // Reset the form after submission (optional)
+    document.getElementById('upload-form').reset();
+}
+
+filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
